d627670abd19413b71dbf27468952df6e0af5228
Daiki67 <ndjambee@gmail.com>
2025-06-18 14:57:56 +0100

    chore: define user's route & admin's route in api.php

--- backend_laravel/routes/api.php
+++ backend_laravel/routes/api.php
@@ -2,7 +2,33 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->prefix('users')->group(function () {
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/users', 'index');
+        Route::get('/users/{id}', 'show');
+        Route::post('/users', 'store');
+        Route::put('/users/{id}', 'update');
+        Route::delete('/users/{id}', 'destroy');
+    });
+
+});
+
+Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
+
+    Route::controller(UserController::class)->prefix('users')->group(function () {
+        Route::get('/users', 'index');
+        Route::get('/users/{id}', 'show');
+        Route::post('/users', 'store');
+        Route::put('/users/{id}', 'update');
+        Route::delete('/users/{id}', 'destroy');
+    });
+
+});
+
+Route::post('/register',[AuthController::class , 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
