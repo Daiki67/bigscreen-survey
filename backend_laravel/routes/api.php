11286120ae68b57bd73f6850644c430dadde2048
Daiki67 <ndjambee@gmail.com>
2025-06-19 14:53:13 +0100

    chore: update api.php

--- backend_laravel/routes/api.php
+++ backend_laravel/routes/api.php
@@ -2,33 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\AuthController;
 
-Route::middleware('auth:sanctum')->prefix('users')->group(function () {
-
-    Route::controller(UserController::class)->group(function () {
-        Route::get('/users', 'index');
-        Route::get('/users/{id}', 'show');
-        Route::post('/users', 'store');
-        Route::put('/users/{id}', 'update');
-        Route::delete('/users/{id}', 'destroy');
-    });
-
-});
 
 Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
 
-    Route::controller(UserController::class)->prefix('users')->group(function () {
-        Route::get('/users', 'index');
-        Route::get('/users/{id}', 'show');
-        Route::post('/users', 'store');
-        Route::put('/users/{id}', 'update');
-        Route::delete('/users/{id}', 'destroy');
-    });
+    /* Route::controller(QuestionController::class)->prefix('questions')->group(function () {
+        Route::get('/question', 'index');
+        Route::get('/question/{id}', 'show');
+        Route::post('/question', 'store');
+        Route::put('/question/{id}', 'update');
+        Route::delete('/question/{id}', 'destroy');
+    }); */
 
 });
 
-Route::post('/register',[AuthController::class , 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
