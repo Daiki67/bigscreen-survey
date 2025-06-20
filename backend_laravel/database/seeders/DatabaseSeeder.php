fbac8f4faa49d21d023bb61b27b97d611a32fcef
Daiki67 <ndjambee@gmail.com>
2025-06-20 10:27:45 +0100

    chore: comment AnswerSeeder::class in DatabaseSeeder.php's call

--- backend_laravel/database/seeders/DatabaseSeeder.php
+++ backend_laravel/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,14 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@gmail.com',
+            'password' => 'password123',
+        ]);
+
+        $this->call([
+            QuestionSeeder::class,
+            /* AnswerSeeder::class, */
+            SubmissionSeeder::class,
         ]);
     }
 }
