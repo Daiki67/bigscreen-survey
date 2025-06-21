8cbe788d60703a0fbc772d30f5a6aa096157b8f9
Daiki67 <ndjambee@gmail.com>
2025-06-21 11:52:06 +0100

    chore: add SurveyController.php's route in api.php

--- backend_laravel/routes/api.php
+++ backend_laravel/routes/api.php
@@ -2,19 +2,40 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\AnswerController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\SurveyController;
 use App\Http\Controllers\AuthController;
 
 
+Route::controller(SurveyController::class)->prefix('survey')->group(function () {
+    Route::get('/question', 'index');
+    Route::post('/store', 'store');
+    Route::get('/result/{token}', 'showResults');
+});
+
 Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
 
-    /* Route::controller(QuestionController::class)->prefix('questions')->group(function () {
+    Route::controller(SurveyController::class)->prefix('survey')->group(function () {
         Route::get('/question', 'index');
-        Route::get('/question/{id}', 'show');
-        Route::post('/question', 'store');
-        Route::put('/question/{id}', 'update');
-        Route::delete('/question/{id}', 'destroy');
-    }); */
+        Route::get('/result/{token}', 'showResults');
+    });
+
+    Route::controller(DashboardController::class)->prefix('questions')->group(function () {
+        Route::get('/submission', 'index');
+        Route::get('/submission/{id}', 'show');
+        Route::post('/submission', 'store');
+        Route::put('/submission/{id}', 'update');
+        Route::delete('/submission/{id}', 'destroy');
+    });
+
+    Route::controller(AnswerController::class)->prefix('questions')->group(function () {
+        Route::get('/answer', 'index');
+        Route::get('/answer/{id}', 'show');
+        Route::post('/answer', 'store');
+        Route::put('/answer/{id}', 'update');
+        Route::delete('/answer/{id}', 'destroy');
+    });
 
 });
 
