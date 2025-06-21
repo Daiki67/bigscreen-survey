b0c334155a6ad4aed6ecb9f95571d6da297e9ccb
Daiki67 <ndjambee@gmail.com>
2025-06-21 16:55:54 +0100

    chore: modify questions's structure in QuestionFactory.php

--- backend_laravel/database/factories/QuestionFactory.php
+++ backend_laravel/database/factories/QuestionFactory.php
@@ -15,7 +15,16 @@ class QuestionFactory extends Factory
             'title'    => $this->faker->sentence,
             'body' => $this->faker->sentence(10),
             'type'     => $this->faker->randomElement(['A', 'B', 'C']),
-            'options'  => $this->faker->randomElement([null, ['Oui', 'Non'], ['A', 'B', 'C']]),
+            'options'  => $this->faker->randomElement([
+                null,
+                ['Oui', 'Non'],
+                [1, 2, 3, 4, 5],
+                ["regarder la TV en direct", "regarder des films", "travailler", "jouer en solo", "jouer en équipe"],
+                ["Occulus Quest", "Occulus Go", "HTC Vive Pro", "PSVR", "Autre", "Aucun"],
+                ["SteamVR", "Occulus store", "Viveport", "Windows store"],
+                ["Oculus Quest", "Oculus Rift/s", "HTC Vive", "Windows Mixed Reality", "Valve index"],
+                ["Homme", "Femme", "Préfère ne pas répondre"]
+            ]),
         ];
     }
 }
