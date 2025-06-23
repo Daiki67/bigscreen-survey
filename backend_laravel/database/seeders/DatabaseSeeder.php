1f7a9463e672d64183c321252b9ab5571a65e607
Daiki67 <ndjambee@gmail.com>
2025-06-23 16:09:41 +0100

    chore: define the name of Admin in DatabaseSeeder.php

--- backend_laravel/database/seeders/DatabaseSeeder.php
+++ backend_laravel/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
+            'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => 'password123',
         ]);
