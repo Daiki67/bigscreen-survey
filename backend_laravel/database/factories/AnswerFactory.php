b0f8c0ff00b53d752457c72e4629f22114ca62e2
Daiki67 <ndjambee@gmail.com>
2025-06-23 16:44:12 +0100

    fix: registering instances of answers in AnswerFactory.php

--- backend_laravel/database/factories/AnswerFactory.php
+++ backend_laravel/database/factories/AnswerFactory.php
@@ -6,6 +6,7 @@ use App\Models\Answer;
 use App\Models\Submission;
 use App\Models\Question;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use PHPUnit\TextUI\Configuration\Merger;
 
 class AnswerFactory extends Factory
 {
@@ -15,37 +16,70 @@ class AnswerFactory extends Factory
     {
         $question = Question::inRandomOrder()->first();
         $value = null;
-        
+
         if ($question?->type === 'A') {
-            $value = $this->faker->randomElement(["Oui", "Non"]);
+
+            if ($question?->id === 3) {
+
+                $value = $this->faker->randomElement([
+                    "Homme",
+                    "Femme",
+                    "Préfère ne pas répondre"
+                ]);
+
+            } elseif($question?->id === 6) {
+
+                $value = $this->faker->randomElement([
+                    "Oculus Quest",
+                    "Oculus Rift/s",
+                    "HTC Vive",
+                    "Windows Mixed Reality",
+                    "Valve index",
+                ]);
+
+            } elseif ($question?->id === 7) {
+
+                $value = $this->faker->randomElement([
+                    "SteamVR",
+                    "Occulus store",
+                    "Viveport",
+                    "Windows store",
+                ]);
+            
+            } elseif( $question?->id === 8) {
+
+                $value = $this->faker->randomElement([
+                    "Occulus Quest",
+                    "Occulus Go",
+                    "HTC Vive Pro",
+                    "PSVR",
+                    "Autre",
+                    "Aucun",
+                ]);
+
+            } elseif ($question?->id === 10) {
+
+                $value = $this->faker->randomElement([
+                    "regarder la TV en direct",
+                    "regarder des films",
+                    "travailler",
+                    "jouer en solo",
+                    "jouer en équipe",
+                ]);
+
+            } else {
+
+                $value = $this->faker->randomElement(["Oui", "Non"]);
+
+            }
         } elseif ($question?->type === 'B') {
-            $value = $this->faker->randomElement([
-                "regarder la TV en direct", 
-                "regarder des films", 
-                "travailler", 
-                "jouer en solo", 
-                "jouer en équipe",
-                "Occulus Quest", 
-                "Occulus Go", 
-                "HTC Vive Pro", 
-                "PSVR", 
-                "Autre", 
-                "Aucun",
-                "SteamVR", 
-                "Occulus store", 
-                "Viveport", 
-                "Windows store",
-                "Oculus Quest", 
-                "Oculus Rift/s", 
-                "HTC Vive", 
-                "Windows Mixed Reality", 
-                "Valve index",
-                "Homme", 
-                "Femme", 
-                "Préfère ne pas répondre"
-            ]);
+
+            $value = $this->faker->sentence(3);
+
         } else {
+
             $value = $this->faker->randomElement([1, 2, 3, 4, 5]);
+            
         }
 
         return [
