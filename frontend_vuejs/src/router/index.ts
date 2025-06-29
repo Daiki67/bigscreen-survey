5180c4902cc5acbaa41ed7034302a4c64839b0df
Daiki67 <ndjambee@gmail.com>
2025-06-30 00:24:55 +0100

    chore: Define all routes of frontend in index.ts

--- frontend_vuejs/src/router/index.ts
+++ frontend_vuejs/src/router/index.ts
@@ -1,22 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SurveyView from '../views/SurveyView.vue'
+import AnswerView from '../views/AnswerView.vue'
+import Administration from '../layout/AdminLayout.vue'
+import DashboardView from '@/views/administration/DashboardView.vue'
+import QuizView from '@/views/administration/QuizView.vue'
+import ResponseView from '@/views/administration/ResponseView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'survey',
+      component: SurveyView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/answer',
+      name: 'answer',
+      component: AnswerView
     },
+    {
+      path: '/administration',
+      component: Administration,
+      children: [
+        { path: '', redirect: { name: 'Dashboard' } },
+        { path: '/dashboard', name: 'Dashboard', component: DashboardView },
+        { path: '/quiz', name: 'Quiz', component: QuizView },
+        { path: '/response', name: 'Response', component: ResponseView },
+      ]
+    }
   ],
 })
 
