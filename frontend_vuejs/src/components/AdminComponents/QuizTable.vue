2fea8a70294874389a49f8d7960f5de33e5b253e
Daiki67 <ndjambee@gmail.com>
2025-06-30 00:19:55 +0100

    chore: Define a QuizTable.vue component for table of Quiz.vue

---
+++ frontend_vuejs/src/components/AdminComponents/QuizTable.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+</script>
+
+<template>
+<table>
+  <thead>
+    <td>N°</td>
+    <td>Corps de la question</td>
+    <td>Type</td>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Votre adresse mail</td>
+      <td>B</td>
+    </tr>
+  </tbody>
+</table>
+</template>
+
+
+<style scoped>
+table {
+  width: 100%;
+  /*border: 1px solid white;*/
+  margin-top: 20px;
+  color: #b3b3b3;
+  border-collapse: collapse;
+}
+
+thead {
+  color: #00b8ff;
+  border: 1px solid #2c2c2c;
+}
+
+thead td,tbody td {
+  padding: 15px;
+  background: #2c2c2c;
+  font-weight: 700;
+  font-size: 1.2rem;
+}
+
+tbody td {
+  background: #1e1e1e;
+  border-bottom: 1px solid #2c2c2c;
+  font-weight: 500;
+}
+</style>
+
