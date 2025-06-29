d3524dccf584dc5783bb7c213a2250817295424d
Daiki67 <ndjambee@gmail.com>
2025-06-30 00:21:51 +0100

    chore: Create Administration's ResponseView.vue page for all users's answers for the survey

---
+++ frontend_vuejs/src/views/administration/ResponseView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Response from '@/components/AdminComponents/Response.vue';
+</script>
+
+<template>
+<section>
+  <Response />
+</section>
+</template>
+
+<style scoped>
+</style>
