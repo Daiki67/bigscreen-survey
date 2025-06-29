70300b7447fe42beeaa2c7531477835984179b84
Daiki67 <ndjambee@gmail.com>
2025-06-30 00:13:58 +0100

    chore: Create a AnswerView.vue page file

---
+++ frontend_vuejs/src/views/AnswerView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import AnswerForm from '@/components/AnswerForm.vue';
+</script>
+
+<template>
+<div class="Answerbody">
+  <AnswerForm />
+</div>
+</template>
+
+<style scoped>
+
+.Answerbody{
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  padding: 40px 0px;
+  background-color: #121212;
+}
+
+</style>
