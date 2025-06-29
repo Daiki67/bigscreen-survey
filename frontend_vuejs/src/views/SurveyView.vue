5c1e449ccdef6a2fd24c4788929d59c92847bb74
Daiki67 <ndjambee@gmail.com>
2025-06-30 00:12:11 +0100

    chore: Create a SurveyView.vue page file

---
+++ frontend_vuejs/src/views/SurveyView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import SurveyForm from '@/components/SurveyForm.vue';
+</script>
+
+<template>
+<div class="Surveybody">
+  <SurveyForm />
+</div>
+</template>
+
+<style scoped>
+
+.Surveybody{
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  padding: 40px 0px;
+  background-color: #121212;
+}
+
+</style>
