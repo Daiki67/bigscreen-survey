4a201847ce82bd79d21decb7a68ea07fb0dd17a6
Daiki67 <ndjambee@gmail.com>
2025-06-30 00:18:02 +0100

    chore: Define a AdminLayout.vue for Administration's personal routes

---
+++ frontend_vuejs/src/layout/AdminLayout.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import Sidebar from '@/components/AdminComponents/Sidebar.vue';
+</script>
+
+<template>
+<div class="LayoutContainer">
+    <Sidebar />
+  <div class="content-wrapper">
+    <router-view />
+  </div>
+</div>
+</template>
+
+<style scoped>
+.LayoutContainer {
+  /*border: 1px solid red;*/
+  width: 100%;
+  height: auto;
+  display: flex;
+}
+
+.content-wrapper {
+  /*border: 2px solid red;*/
+  margin-left: 23%;
+  width: calc(100% - 23%);
+  height: 100vh;
+  background-color: #121212;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  overflow-x: hidden;
+}
+</style>
