73cc141fb0ee3f10c4642d54c8d79859cb95eb49
Daiki67 <ndjambee@gmail.com>
2025-06-30 00:15:13 +0100

    chore: Create a DashboardView.vue page file for Administration

---
+++ frontend_vuejs/src/views/administration/DashboardView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Dashboard from '@/components/AdminComponents/Dashboard.vue';
+</script>
+<template>
+<section>
+  <Dashboard />
+</section>
+</template>
+
+<style scoped>
+</style>
