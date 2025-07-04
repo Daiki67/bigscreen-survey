9ea3b8b61fe9c0a1ebaa3a1e7c02d8ae743fa3f5
Daiki67 <ndjambee@gmail.com>
2025-07-04 14:21:29 +0100

    feat: define the component of radar chart in Dashboard.vue

--- frontend_vuejs/src/components/AdminComponents/Dashboard.vue
+++ frontend_vuejs/src/components/AdminComponents/Dashboard.vue
@@ -1,5 +1,81 @@
 <script setup>
+import { onMounted, ref } from 'vue';
 import ChartCard from './ChartCard.vue';
+import axios from '@/utilities/axios.js';
+import { getAccessToken } from '@/utilities/utils.js' ;
+
+const errorMessage = ref('');
+const PieChartDataQuestion6 = ref([]);
+const PieChartDataQuestion7 = ref([]);
+const PieChartDataQuestion10 = ref([]);
+const RadarChartData = ref([]);
+
+const fetchDataChart = async () => {
+  try {
+    const response = await axios.get('/admin/dashboard/datacharts',{
+      headers: { Authorization: 'Bearer ' + getAccessToken() }
+    });
+    PieChartDataQuestion6.value = response.data.PieChartData.question_6;
+    PieChartDataQuestion7.value = response.data.PieChartData.question_7;
+    PieChartDataQuestion10.value = response.data.PieChartData.question_10;
+    RadarChartData.value = response.data.RadarChartData;
+
+    /* console.log(PieChartDataQuestion6.value);
+    console.log(PieChartDataQuestion7.value);
+    console.log(PieChartDataQuestion6.value); */
+    console.log(RadarChartData.value);
+  } catch (e) {
+    console.error('Erreur des données des graphiques', e);
+    errorMessage.value = 'Erreur survenue lors de la récupération des données du graphique';
+    alert(errorMessage.value);
+  }
+}
+
+/* var options = {
+            series: [5, 5, 2, 3],
+            chart: {
+            width: 700,
+            type: 'pie',
+          },
+          labels: ['Ordinateur', 'Smartphone', 'Tablette', 'Télévision'],
+          responsive: [{
+            breakpoint: 380,
+            options: {
+              chart: {
+                width: 500
+              },
+              legend: {
+                position: 'bottom'
+              }
+            }
+          }]
+        }; */
+
+const RadarDataOptions = ref({
+        series: [{
+          name: 'Series 1',
+          data: /* [3.75, 4.5, 3.5, 4.5, 4.5] */
+            RadarChartData.value.forEach((value, count) => {
+              [ count ]
+            })
+          ,
+        }],
+        options:{
+          chart: {
+            height: 850,
+            width: 500,
+            type: 'radar',
+          },
+          /* title: {
+            text: 'Quality Aspects of Bigscreen'
+          }, */
+          xaxis: {
+            categories: ['Image Quality', 'Interface Comfort', 'Network Connection', '3D Graphics Quality', 'Audio Quality']
+          }
+      },
+});
+
+onMounted(fetchDataChart);
 </script>
 
 <template>
@@ -9,17 +85,29 @@ import ChartCard from './ChartCard.vue';
     <hr>
   </article>
   <article class="DashboardContent">
-    <ChartCard
+    <!-- <ChartCard
+      ChartType="pie"
+      ChartOptions="d"
+      ChartSeries="d"
       ChartTitle="Casques VR utilisés (Q6)"
     />
     <ChartCard
+      ChartType="pie"
+      ChartOptions="d"
+      ChartSeries="d"
       ChartTitle="Magasins d'applications (Q7)"
     />
     <ChartCard
+      ChartType="pie"
+      ChartOptions="d"
+      ChartSeries="d"
       ChartTitle="Utilisation principale (Q10)"
-    />
+    /> -->
     <ChartCard
-      ChartTitle="Qualité de l'expérience (Q11-15)"
+      :ChartType="RadarDataOptions.options.chart.type"
+      :ChartOptions="RadarDataOptions.options"
+      :ChartSeries="RadarDataOptions.series"
+      ChartTitle="Pour les questions (Q11-Q15)"
     />
   </article>
 </section>
