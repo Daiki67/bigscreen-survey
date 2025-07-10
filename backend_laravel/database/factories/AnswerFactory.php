32a0c79b1283d852d5fa1e357d8237a0c8bc1935
Daiki67 <ndjambee@gmail.com>
2025-07-11 00:26:31 +0100

    fix: modify AnswerFactory.php to allow AnswerSeeder.php for data of answer's seeder

--- backend_laravel/database/factories/AnswerFactory.php
+++ backend_laravel/database/factories/AnswerFactory.php
@@ -14,78 +14,14 @@ class AnswerFactory extends Factory
 
     public function definition(): array
     {
-        $question = Question::inRandomOrder()->first();
         $value = null;
 
-        if ($question?->type === 'A') {
-
-            if ($question?->id === 3) {
-
-                $value = $this->faker->randomElement([
-                    "Homme",
-                    "Femme",
-                    "Préfère ne pas répondre"
-                ]);
-
-            } elseif($question?->id === 6) {
-
-                $value = $this->faker->randomElement([
-                    "Oculus Quest",
-                    "Oculus Rift/s",
-                    "HTC Vive",
-                    "Windows Mixed Reality",
-                    "Valve index",
-                ]);
-
-            } elseif ($question?->id === 7) {
-
-                $value = $this->faker->randomElement([
-                    "SteamVR",
-                    "Occulus store",
-                    "Viveport",
-                    "Windows store",
-                ]);
-            
-            } elseif( $question?->id === 8) {
-
-                $value = $this->faker->randomElement([
-                    "Occulus Quest",
-                    "Occulus Go",
-                    "HTC Vive Pro",
-                    "PSVR",
-                    "Autre",
-                    "Aucun",
-                ]);
-
-            } elseif ($question?->id === 10) {
-
-                $value = $this->faker->randomElement([
-                    "regarder la TV en direct",
-                    "regarder des films",
-                    "travailler",
-                    "jouer en solo",
-                    "jouer en équipe",
-                ]);
-
-            } else {
-
-                $value = $this->faker->randomElement(["Oui", "Non"]);
-
-            }
-        } elseif ($question?->type === 'B') {
-
-            $value = $this->faker->sentence(3);
-
-        } else {
-
-            $value = $this->faker->randomElement([1, 2, 3, 4, 5]);
-            
-        }
-
         return [
-            'submission_id' => Submission::inRandomOrder()->first()?->id,
-            'question_id'   => $question?->id,
+
+            'submission_id' => Submission::factory(),
+            'question_id'   => Question::factory(),
             'value'         => $value,
+
         ];
     }
 }
