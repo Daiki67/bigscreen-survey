9a5c8e195360bee6aa4239236c8c8cad29110d4d
Daiki67 <ndjambee@gmail.com>
2025-07-22 01:48:17 +0100

    feat: add general loader for all pages that's use axios for request in App.vue

--- frontend_vuejs/src/App.vue
+++ frontend_vuejs/src/App.vue
@@ -1,13 +1,32 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+<script setup>
+import { ref, provide, onMounted } from 'vue'
+import Loader from '@/components/Loader.vue'
+import { setGlobalLoadingSetter } from './utilities/axios.js'
+
+// Variable réactive globale pour le chargement
+const isLoading = ref(false)
+
+// Fournit la variable et la fonction pour les composants enfants
+provide('isLoading', isLoading)
+provide('setLoading', (val) => { isLoading.value = val })
+
+// Connecte le loader à Axios au montage de l'app
+onMounted(() => {
+  setGlobalLoadingSetter((val) => { isLoading.value = val })
+})
 </script>
 
 <template>
-  <div>
+  <div id="app">
+    <Loader v-if="isLoading" />
     <router-view />
   </div>
 </template>
 
 <style scoped>
-
+#app{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 </style>
