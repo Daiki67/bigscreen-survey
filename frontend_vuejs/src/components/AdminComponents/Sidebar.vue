68e957f6a8ebdb7e34cecbabb19a92b2a85aef95
Daiki67 <ndjambee@gmail.com>
2025-07-22 01:53:07 +0100

    chore: add TopMenu in Sidebar.vue for phone's responsive

--- frontend_vuejs/src/components/AdminComponents/Sidebar.vue
+++ frontend_vuejs/src/components/AdminComponents/Sidebar.vue
@@ -35,6 +35,7 @@ import { getAccessToken, removeAccessToken } from '@/utilities/utils.js'; // Fon
 
 // errorMessage : ref<string> - message d'erreur pour la déconnexion
 const errorMessage = ref('');
+const showMenu = ref(false);
 // router : instance du routeur pour navigation programmatique
 const router = useRouter();
 
@@ -61,7 +62,13 @@ const handleLogout = async () => {
   }
 }
 
+const isOpen = () => {
+  showMenu.value = !showMenu.value; 
+}
 
+const closeMenu = () => {
+  showMenu.value = false;
+}
 </script>
 
 <template>
@@ -154,10 +161,52 @@ const handleLogout = async () => {
     </div>
 
     <div  class="TopBar">
-      <h2>Administration</h2>
+      <div class="TopBarTitle">
+        <router-link class="routerlink" to="/admin">
+          bigscreen
+        </router-link>
+      </div>
+      <div class="TopBarMenu" @click="isOpen">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+
+    <div class="PhoneMenu" v-if="showMenu" :class="{ 'nav-open': isOpen }">
+      
+      <router-link to="/administration/dashboard" class="router-link" @click.native="closeMenu">
+        <button type="button" :class="{ 'active' : $route.name === 'Dashboard' }">
+          Acceuil
+        </button>
+      </router-link>
+
       <hr>
+      
+      <router-link to="/administration/quiz" class="router-link" @click.native="closeMenu">
+        <button type="button" :class="{ 'active' : $route.name === 'Quiz' }">
+          Questionnaire
+        </button>
+      </router-link>
+
+      <hr>
+      
+      <router-link to="/administration/response" class="router-link" @click.native="closeMenu">
+        <button type="button" :class="{ 'active' : $route.name === 'Response' }">
+          Réponses
+        </button>
+      </router-link>
+
+      <hr>
+      
+      <router-link to="#" class="router-link" @click="handleLogout" @click.native="closeMenu">
+        <button type="button">
+          Logout
+        </button>
+      </router-link>
 
     </div>
+
   </div>
 </template>
 
@@ -172,7 +221,8 @@ const handleLogout = async () => {
   align-items: center;
 }
 
-.TopBar {
+.TopBar,
+.PhoneMenu {
   display: none;
 }
 
@@ -271,6 +321,103 @@ button.active:hover {
   .SidebarContainer {
     display: none;
   }
+
+  .TopBar {
+    position: absolute;
+    top: 0;
+    height: 60px;
+    width: 100%;
+    background-color: #00b8ff;
+    display: flex;
+    z-index: 999;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .TopBarTitle {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.8rem;
+    margin-left: 20px;
+    font-family: 'Times New Roman', Times, serif;
+  }
   
+  .routerlink {
+    color: #fff;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  .TopBarMenu {
+    border: 1.5px solid white;
+    width: 50px;
+    height: 35px;
+    margin-right: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    border-radius: 8px;
+  }
+
+  .TopBarMenu div {
+    height: 2px;
+    width: 70%;
+    background-color: #fff;
+  }
+
+  .PhoneMenu {
+    position: absolute;
+    z-index: 999;
+    background-color: #2c2c2c;
+    width: 100%;
+    height: 100vh;
+    top: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: start;
+    transform: translateX(-100%);
+    transition: transform 0.3s ease-in-out;
+  }
+
+  .PhoneMenu.nav-open{
+    transform: translateX(0);
+  }
+
+  .PhoneMenu hr {
+    border: 1px solid #00b8ff;
+    width: 90%;
+    margin-bottom: 30px;
+  }
+
+  .PhoneMenu .router-link {
+    /*border: 1px solid red;*/
+    margin-bottom: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .PhoneMenu .router-link:nth-child(1) {
+    margin-top: 100px;
+  }
+  
+  .PhoneMenu .router-link button {
+    padding: 14px 15px;
+    width: 90%;
+    border-radius:8px;
+    font-size: 1.4rem;
+    text-align: left;
+    background-color: #1e1e1e;
+    border: none;
+    color: #b3b3b3;
+  }
+
+  .PhoneMenu .router-link button.active {
+    background-color: #00b8ff;
+  } 
+
 }
 </style>
